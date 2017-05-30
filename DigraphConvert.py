8fc92ae8190b3ab5203195c7010365b1c10094ec
Charlie Fieseler <Charlie Fieseler>
2017-05-31 00:47:35 +0100

    Weird bug with the c elegans connectome starting at 0

--- DigraphConvert.py
+++ DigraphConvert.py
@@ -1,35 +1,36 @@
 
 # coding: utf-8
 
-# In[3]:
+# In[2]:
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
-# # Create Sample Digraph (Fig. 1h from Liu (2011))
-# DG = nx.DiGraph()
-# DG.add_nodes_from(range(1,7)) #add 6 nodes
-# DG.nodes()
-# DG.add_edges_from([(1,2),(1,3),(1,4),(1,5),(1,6),(3,4)])
-# #nx.draw(DG, with_labels=True)
-# # print('In edges: ')
-# # DG.in_edges()
-# # print('Out edges: ')
-# # DG.out_edges()
+# Create Sample Digraph (Fig. 1h from Liu (2011))
+DG = nx.DiGraph()
+DG.add_nodes_from(range(1,7)) #add 6 nodes
+DG.nodes()
+DG.add_edges_from([(1,2),(1,3),(1,4),(1,5),(1,6),(3,4)])
+#nx.draw(DG, with_labels=True)
+# print('In edges: ')
+# DG.in_edges()
+# print('Out edges: ')
+# DG.out_edges()
 
 def digraphToBipartite(digIn):
     n = digIn.number_of_nodes()
     bipOut = nx.Graph()
-    bipOut.add_nodes_from(range(1,2*n+1)) #first n are out nodes, second n are in nodes
+    bipOut.add_nodes_from(range(1,n+1), bipartite=0) #first n are out nodes, second n are in nodes
+    bipOut.add_nodes_from(range(n+1,2*n+1), bipartite=1) #outs have bipartite flag 0, ins have bipartite flag 1
     diEdges = digIn.edges()
     for j in diEdges:
         bipOut.add_edge(j[0],j[1]+n) #add edge from output subgraph to input subgraph
     return bipOut
 
-# BP = digraphToBipartite(DG)
-# print('Bipartite Output:')
-# print(BP.nodes())
-# print(BP.edges())
+BP = digraphToBipartite(DG)
+print('Bipartite Output:')
+print(BP.nodes())
+print(BP.edges())
 
 def bipartiteToDigraph(bipIn):
     n = bipIn.number_of_nodes() * 0.5
@@ -45,10 +46,10 @@ def bipartiteToDigraph(bipIn):
         digOut.add_edge(j[0],j[1]-n)
     return digOut
 
-# DG_Reconstituted = bipartiteToDigraph(BP)
-# print('Digraph Output:')
-# print(DG_Reconstituted.nodes())
-# print(DG_Reconstituted.edges())
+DG_Reconstituted = bipartiteToDigraph(BP)
+print('Digraph Output:')
+print(DG_Reconstituted.nodes())
+print(DG_Reconstituted.edges())
 
 #mm = [{1:6,2:7,3:8},{1:7,4:5,3:8}]
 
@@ -64,6 +65,22 @@ def B2Dmatching(mm,n):
         dgmm.append(thisMM)
     return dgmm
 
+def CelegansLabelConvert(G):
+    '''
+    Takes a graph that doesn''t start at node label 1 and relabels them with integers
+    
+    Specifically for use with the C elegans connectome
+    '''
+    
+    nodeList = G.nodes()
+    labelDict = dict(zip(range(len(nodeList)), nodeList))
+
+    H = nx.convert_node_labels_to_integers(G)
+    nx.set_node_attributes(H, 'neuronLabel', labelDict)
+    #Also write the text file for future saving
+    #nx.write_graphml(H, 'elegansGraphML.txt',encoding='utf-8', prettyprint=True)
+    
+    return H
 
 
 # In[ ]:
