62858026cbaeae788cddd1ab3b2b4f505772f97b
ded114 <dandylewsky@gmail.com>
2017-05-30 19:52:37 -0700

    Fixed Start-From-0 Errror in TestNetworks
    Also saved new .py versions of TestNetworks and DigraphConvert

--- DigraphConvert.py
+++ DigraphConvert.py
@@ -1,16 +1,16 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[5]:
 
 import networkx as nx
 import matplotlib.pyplot as plt
 
 # Create Sample Digraph (Fig. 1h from Liu (2011))
 DG = nx.DiGraph()
-DG.add_nodes_from(range(1,7)) #add 6 nodes
+DG.add_nodes_from(range(6)) #add 6 nodes
 DG.nodes()
-DG.add_edges_from([(1,2),(1,3),(1,4),(1,5),(1,6),(3,4)])
+DG.add_edges_from([(0,1),(0,2),(0,3),(0,4),(0,5),(2,3)])
 #nx.draw(DG, with_labels=True)
 # print('In edges: ')
 # DG.in_edges()
@@ -20,8 +20,8 @@ DG.add_edges_from([(1,2),(1,3),(1,4),(1,5),(1,6),(3,4)])
 def digraphToBipartite(digIn):
     n = digIn.number_of_nodes()
     bipOut = nx.Graph()
-    bipOut.add_nodes_from(range(1,n+1), bipartite=0) #first n are out nodes, second n are in nodes
-    bipOut.add_nodes_from(range(n+1,2*n+1), bipartite=1) #outs have bipartite flag 0, ins have bipartite flag 1
+    bipOut.add_nodes_from(range(0,n), bipartite=0) #first n are out nodes, second n are in nodes
+    bipOut.add_nodes_from(range(n,2*n), bipartite=1) #outs have bipartite flag 0, ins have bipartite flag 1
     diEdges = digIn.edges()
     for j in diEdges:
         bipOut.add_edge(j[0],j[1]+n) #add edge from output subgraph to input subgraph
@@ -40,7 +40,7 @@ def bipartiteToDigraph(bipIn):
         return
     n = int(n)
     digOut = nx.DiGraph()
-    digOut.add_nodes_from(range(1,n+1))
+    digOut.add_nodes_from(range(0,n))
     bipEdges = bipIn.edges()
     for j in bipEdges:
         digOut.add_edge(j[0],j[1]-n)
@@ -73,7 +73,9 @@ def CelegansLabelConvert(G):
     '''
     
     nodeList = G.nodes()
-    labelDict = dict(zip(range(len(nodeList)), nodeList))
+    labelDict = dict(zip(range(1,len(nodeList)), nodeList))
+    
+    #print(labelDict)
 
     H = nx.convert_node_labels_to_integers(G)
     nx.set_node_attributes(H, 'neuronLabel', labelDict)
@@ -81,6 +83,19 @@ def CelegansLabelConvert(G):
     #nx.write_graphml(H, 'elegansGraphML.txt',encoding='utf-8', prettyprint=True)
     
     return H
+
+
+# In[30]:
+
+import TestNetworks as tn
+
+G = tn.testNet(100)
+
+G = CelegansLabelConvert(G)
+print(G.nodes())
+Gbi = digraphToBipartite(G)
+
+#print(Gbi.nodes(data=True))
 
 
 # In[ ]:
